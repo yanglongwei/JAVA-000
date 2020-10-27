ca4f7c58e1be00a68486bb5cc36ba50583416727
yanglongwei <yanglongwei1606@163.com>
2020-10-27 21:53:22 +0800

    gc

---
+++ Week_02/java/src/main/java/com/geek/java/HttpClientSender.java
@@ -0,0 +1,33 @@
+package com.geek.java;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class HttpClientSender {
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        //2.声明get请求
+        HttpGet httpGet = new HttpGet("http://localhost:8801/");
+        //3.发送请求
+        CloseableHttpResponse response = httpClient.execute(httpGet);
+        //4.判断状态码
+        if(response.getStatusLine().getStatusCode()==200){
+            HttpEntity entity = response.getEntity();
+            //使用工具类EntityUtils，从响应中取出实体表示的内容并转换成字符串
+            String string = EntityUtils.toString(entity, "utf-8");
+            System.out.println(string);
+        }
+        //5.关闭资源
+        response.close();
+        httpClient.close();
+    }
+
+
+}
